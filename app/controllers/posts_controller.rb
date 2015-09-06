e7b6fd0b85024f2b39b0cd83fcc34c2992aa18cb
Richard Wilkerson <richard@time2track.com>
2015-09-06 12:20:24 -0400

    Linked M:M Posts & Category by PostCategory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
   def index
+    render 'posts/index'
+  end
+
+  def new
 
   end
+
+  def show
+    @post = Post.find(params[:id])
+    render :show
+  end
+
 end
